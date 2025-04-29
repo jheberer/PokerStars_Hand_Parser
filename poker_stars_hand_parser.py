66bc4fa79d6667a67bd30fff5533ee4c0b89f382
jheberer <jrheberer@gmail.com>
2025-04-29 17:00:19 -0400

    This file successfully creates a dictionary of hands, with key of the dict as the first line of the hand history

--- poker_stars_hand_parser.py
+++ poker_stars_hand_parser.py
@@ -10,19 +10,12 @@ with open(hand_history_path, 'r', encoding='utf-8-sig') as file:
 hands_list = table_contents.split('\n\n\n\n')
 
 # delete the last "hand"--it's a lonely new line
-# del hands_list[-1]
+del hands_list[-1]
 
 hand_list_length = len(hands_list)
 
 for hand in hands_list:
-    print('_________NEW____________')
     hand_breakdown = hand.split('\n')
-    print(hand_breakdown)
-    # for item in hand:
-    #     print("-------------------NEW----------------------")
-    #     print(hand_breakdown)
+    hands[hand_breakdown[0]] = hand_breakdown[1:]
 
-    # get hand index. this is static
-    # uq_hand_id_index_start = hand.index('#')
-    # uq_hand_id_index_end = hand.index('#')
-    # print(hand[uq_hand_id_index+1])
+print(hands["PokerStars Hand #255768010672:  Hold'em No Limit ($0.02/$0.05 USD) - 2025/04/29 14:08:59 ET"])
