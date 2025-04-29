56a8d23d2aacefa650458ca3e22879aee191989b
jheberer <jrheberer@gmail.com>
2025-04-29 16:57:16 -0400

    This is an attempt to open the file and split using newline chars

--- poker_stars_hand_parser.py
+++ poker_stars_hand_parser.py
@@ -4,14 +4,25 @@ hand_history_path = r"C:\Users\jrheb\AppData\Local\PokerStars.USMI\HandHistory\O
 hands = {}
 
 with open(hand_history_path, 'r', encoding='utf-8-sig') as file:
-    table_contents = file.readlines()
+    table_contents = file.read()
 
-for row in table_contents:
-    line = row.split()
+# hands are separated by 4 new lines
+hands_list = table_contents.split('\n\n\n\n')
 
-    if len(line) > 0:
-        # first word of line
-        first_word = line[0]
-        
-        if first_word == 'PokerStars':
-            print('Start of Hand')
+# delete the last "hand"--it's a lonely new line
+# del hands_list[-1]
+
+hand_list_length = len(hands_list)
+
+for hand in hands_list:
+    print('_________NEW____________')
+    hand_breakdown = hand.split('\n')
+    print(hand_breakdown)
+    # for item in hand:
+    #     print("-------------------NEW----------------------")
+    #     print(hand_breakdown)
+
+    # get hand index. this is static
+    # uq_hand_id_index_start = hand.index('#')
+    # uq_hand_id_index_end = hand.index('#')
+    # print(hand[uq_hand_id_index+1])
